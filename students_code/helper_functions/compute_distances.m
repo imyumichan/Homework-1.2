ec7064ec3645388556904822569c56557b11b6d1
Yumi Chan <chanto@in.tum.de>
2019-12-11 07:17:47 +0100

    Fix distance calculation

--- students_code/helper_functions/compute_distances.m
+++ students_code/helper_functions/compute_distances.m
@@ -1,12 +1,7 @@
 function dists = compute_distances(points_2d,true_2d)
 
       f = [points_2d(1,:)'; points_2d(2,:)'];
-
-
       m = [true_2d(1,:)'; true_2d(2,:)'];
-      
-      dists = f-m;
-
-
+      dists = abs(f-m);
 
 end
